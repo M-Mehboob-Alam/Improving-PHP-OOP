e2279d3188560d5a5062da25f3b0bac270dc70bb
Mehboob Alam <mehboobalam.dev@gmail.com>
2025-07-03 11:50:53 +0500

    positioned and named arguments

---
+++ classes/PositionedNamedArguments.php
@@ -0,0 +1,11 @@
+<?php
+class PositionedNamedArgument{
+    public $dollarPrice = 285;
+  
+    public function prkToDollarConverter( $amount = 50000, $symbol = "$" ){
+          $convertedCurrency =  $amount /$this->dollarPrice ;
+          return $symbol . $convertedCurrency;
+    }
+}
+$converter = new PositionedNamedArgument();
+echo $converter->prkToDollarConverter( "Rs." );
